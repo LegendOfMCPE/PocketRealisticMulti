c1922155037590edb09037f1c0548f34a7af7d9e
PEMapModder <pemapmodder1970@gmail.com>
2014-02-04 22:54:16 +0800

    C:/Program Files/Git/ok

--- PRMAPI.php
+++ PRMAPI.php
@@ -16,19 +16,52 @@ class PRMAPI{
 		foreach($this->server->api->player->getAll() as $player){
 			if($player->entity->level->getName() === $pos->level->getName()){
 				$dist=$pos->distance($player->entity);
-				$chance=$volume*100/$dist;
+				
+				
+				$chance=$volume*100/$dist; // TODO Improvement on this chance mechanic
+				
+				
 				$chance=min($chance, 100);
 				for($i=0;$i<strlen($msg);$i++){
-					if($rand(0, 100) > $msg)
+					if($rand(0, 99) > $msg)
 						$msg=substr($msg, 0, $i) . "?" . substr($msg, $i+1);
 				}
-				if(!(strlen(str_replace("?", "", $msg)) < 2 or strlen($om) / strlen(str_replace("?", "", $msg)) > 10)) {// if less than two characters or 10% of the original message is delivered, do not annoy the player
+				if(!(strlen(str_replace("?", "", $msg)) < 2 or strlen($om) / strlen(str_replace("?", "", $msg)) > 10)) { // if less than two characters or 10% of the original message is delivered, do not annoy the player
 					if($player instanceof Player)
 						$msg="$player: $msg";
 					$player->sendChat($msg);
 				}
 			}
 		}
+	}
+	//entity utils
+	public function getEntityName(Entity $e){
+		$target="entity";
+		$testPlayer=$this->api->player->getByEID($e->getEID());
+		if($testPlayer instanceof Player)
+			$target="$testPlayer";
+		else{
+			switch($e->type){
+				case MOB_ZOMBIE:$target="the zombie";
+				break;case MOB_CREEPER:$target="the creeper";
+				break;case MOB_SKELETON:$target="the skeleton";
+				break;case MOB_SPIDER:$target="the spider";
+				break;case MOB_PIGMAN:$target="the zombie pigman";
+				break;case MOB_CHICKEN:$target="the chicken";
+				break;case MOB_COW:$target="the cow";
+				break;case MOB_PIG:$target="the pig";
+				break;case MOB_SHEEP:
+					$color=$this->getColorById($e->data["Color"]);
+					$target="the ".$color." sheep";
+			}
+		}
+		return $target;
+	}
+	public function getColorById($c){
+		if($c==0)return "white";
+		if($c==15)return "black";
+		return "";
+		// TODO more colours
 	}
 }
 class PlayerRecord extends Config{
--- RealisticMPPluginMaster.php
+++ RealisticMPPluginMaster.php
@@ -13,14 +13,17 @@ define("PRM_VOL_WHISPER", 2);
 define("PRM_VOL_SHOUT", 8);
 define("PRM_VOL_YELL", 10);
 define("PRM_VOL_MAX", 12);
+define("PRM_OKSTATUS_CANCELLED", 0x80);
+define("PRM_OKSTATUS_PULL", 0x81);
 
 class PocketRealMultiPlugin implements Plugin{
 	public static $inst;
 	public static function get(){
 		return (isset(self::$inst)?(self::$inst):false);
 	}
-	
+	public $heldItems=array();
 	private $api;
+	private $playersOkStatus=array();
 	public function __construct(ServerAPI $a,$s=0){
 		ServerAPI::request()->loadAPI("prm", "PRMAPI", FILE_PATH."plugins/");
 		self::$inst=$this;
@@ -30,14 +33,48 @@ class PocketRealMultiPlugin implements Plugin{
 	public function init(){
 		$s=ServerAPI::request();
 		$a=$s->api;
-		$s->addHandler("player.chat", array($this,"evRed"));
+		$s->addHandler("player.chat", array($this,"evt"));
+		$s->addHandler("player.interact", array($this,"evt"));
+		$s->addHandler("player.equipment.change", array($this,"evt"));
 	}
-	public function evRed($d,$inEvt){
+	public function evt($d,$inEvt){
 		switch($inEvt){
 			case "player.chat":
 				$msg=$d["message"];
 				$this->api->prm->announceAt($d["player"]->entity, $msg, PRM_VOL_NORM, $d["player"]);
 				return false;
+			case "player.interact":
+				$item=$this->heldItems[$d["player"]->username];
+				if($item->getID()===287 and $item->count >= ((int)($d["player"]->entity->distance($d["target"])))){
+					$player=$this->api->player->getByEID($d["target"]->getEID());
+					if($player instanceof Player){
+						$player->sendChat($d["player"]->username." wants to pull you to him with a string.\nAgree? Use /ok to accept pulling.");
+						$this->playerOkStatus[$player->username]=array(PRM_OKSTATUS_PULL, $d["player"]);
+					}
+					else{
+						$d["target"]->setPosition($d["player"]->entity);
+						$d["player"]->sendChat("Successfully pulled a non-player entity to you.");
+					}
+				}elseif($item->getID()===287){
+					$target=$this->api->prm->getEntityName($d["target"]);
+					$d["player"]->sendChat("You failed to pull $target because the string you are using is too short. A minimum of ".((int)($d["player"]->entity->distance($d["target"])))." of string is needed.");
+				}
+				break;
+			case "player.equipment.change":
+				$this->heldItems[$d["player"]->username]=$d["item"];
+			case "console.command":
+				if($d["cmd"]==="ok" and ($d["issuer"] instanceof Player)){
+					if(!isset($this->playersOkStatus[$d["issuer"]->username]) or ($data=$this->playersOkStatus[$d["issuer"]->username])[0]===PRM_OKSTATUS_CANCELLED)
+						$this->api->handle("player.chat", array("player"=>$d["issuer"], "message"=>"ok"));
+					switch($data[0]){
+						case PRM_OKSTATUS_PULL:
+							$d["issuer"]->teleport($data[1]->entity);
+							$d["issuer"]->sendChat("You have been pulled to ".$data[1]." via string.");
+							break;
+					}
+				}
 		}
+		
 	}
+	
 }
