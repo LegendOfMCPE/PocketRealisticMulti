6fbada94774668fe40f5803b81655eefd6568731
PEMapModder <pemapmodder1970@gmail.com>
2014-03-13 17:25:23 +0800

    Add files

--- PocketRealisticMulti.php
+++ PocketRealisticMulti.php
@@ -10,10 +10,16 @@ author=PEMapModder
 */
 
 class PrmMainPlugin{
-	public $api, $server;
-	private static $instance = false;
+	public $api, $server, $main;
+	private static $instance=false;
 	private static function setInstance(self &$i){
 		self::$instance=&$i;
+	}
+	public static function get(){
+		return self::$instance;
+	}
+	public static function request(){
+		return self::get()->main;
 	}
 	public function __construct(ServerAPI $api, $s=0){
 		self::setInstance(&$this);
@@ -24,7 +30,7 @@ class PrmMainPlugin{
 	public function __destruct(){
 	}
 	public function init(){
-		
+		$this->main=new PRMMain();
 	}
 	public function requireAll(){
 		$path=FILE_PATH."plugins/PocketRealisticMulti/code/classes/";
--- PocketRealisticMulti/code/classes/PocketRealisticMulti_Main.php
+++ PocketRealisticMulti/code/classes/PocketRealisticMulti_Main.php
@@ -1,4 +1,8 @@
 <?php
 
-class Main{
+class PRMMain{
+	public function __construct(){
+		$this->initEvts();
+		$this->initCmds();
+	}
 }
