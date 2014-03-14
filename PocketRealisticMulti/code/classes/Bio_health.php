980a7ba55e89a28f73823e5a69f528ce7b85ca05
PEMapModder <pemapmodder1970@gmail.com>
2014-03-15 00:26:53 +0800

    Untitled commit (I am too lazy to type)

--- PocketRealisticMulti/code/classes/Bio_health.php
+++ PocketRealisticMulti/code/classes/Bio_health.php
@@ -1,4 +1,19 @@
 <?php
 
+class PrmHealthHandler{
+	public $healths=array();
+	public function __construct(){
+		ServerAPI::request()->addHandler("player.spawn", array($this, "add"));
+		ServerAPI::request()->addHandler("player.quit", array($this, "rm"));
+		ServerAPI::request()->addHandler("player.respawn", array($this, "add"));
+		// ServerAPI::request()->addHandler("player.death", array($this, "rm"));
+	}
+	public function add(Player $player){
+		$this->healths[$player->iusername]=new Health($player);
+	}
+	public function rm(Player $player){
+		unset($this->healths[$player->iusername]);
+	}
+}
 class PrmHealth{
 }
--- PocketRealisticMulti/code/classes/Phys_sound.php
+++ PocketRealisticMulti/code/classes/Phys_sound.php
@@ -1,6 +1,15 @@
 <?php
 
 class PrmSoundHandler{
+	public function __construct(){
+		ServerAPI::request()->addHandler("player.chat", array($this, "onChat"));
+	}
+	public function onChat($data){
+		$sound=new PrmSoundWave($data["player"]->entity, PrmSoundWave::TYPE_MESSAGE, $data["message"]
+	}
 }
 class PrmSoundWave{
+	const TYPE_MESSAGE=0;
+	public function __construct(Position $origin, $type, $data, $magnitude=5){
+	}
 }
