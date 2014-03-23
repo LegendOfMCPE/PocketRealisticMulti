394394a339a8191444c652b67a211aeb37838ae8
PEMapModder <pemapmodder1970@gmail.com>
2014-03-23 22:17:35 +0800

    Untitled commit (I am too lazy to title it :D)

--- PocketRealisticMulti/code/classes/Main.php
+++ PocketRealisticMulti/code/classes/Main.php
@@ -1,15 +1,27 @@
 <?php
 
 class PRMMain{
-	$items=array();
+	public $items=array();
+	public $players=array();
 	public function __construct(){
 		PrmMainPlugin::get()->phys=new PrmPhys();
 		PrmMainPlugin::get()->bio=new PrmBio();
 		PrmMainPlugin::get()->soc=new PrmSoc();
 		ServerAPI::request()->addHandler("player.equipment.change", array($this, "changeItem"));
+		ServerAPI::requeset()->addHandler("player.join", array($this, "onJoin"));
+		define("PRM_FILE_DIR", FILE_PATH."plugins/PocketRealisticMulti/saved-data/");
+		@mkdir(PRM_FILE_DIR);
+		define("PRM_PLAYERS_DIR", PRM_FILE_DIR."players/");
+		@mkdir(PRM_PLAYERS_DIR);
 	}
 	public function changeItem($data){
 		$this->items[$data["player"]->iusername]=$data["item"];
+	}
+	public function onJoin(Player $data){
+		$this->players[$data->iusername]=new Config(PRM_PLAYERS_DIR.$data->iusername.".yml", CONFIG_YAML, array("Main"=>array(),));
+		if(!file_exists(PRM_PLAYERS_DIR.$data->iusername.".yml"))
+			ServerAPI::request()->dhandle("prm.new.player", $data);
+		else ServerAPI::request()->dhandle("prm.old.player", $data);
 	}
 	public function getCarriedItem(Player $player){
 		return $this->items[$player->iusername];
--- PocketRealisticMulti/code/classes/Soc_philo_birth.php
+++ PocketRealisticMulti/code/classes/Soc_philo_birth.php
@@ -1,4 +1,33 @@
 <?php
 
 class PrmPhiloBirth{
+	public function __construct(){
+		define("PRM_BIRTH_DIR", PRM_FILE_DIR."birth/");
+		ServerAPI::request()->addHandler("prm.new.player", array($this, "onJoin"));
+		ServerAPI::request()->addHandler("player.spawn", array($this, "onSpawn"));
+		$this->dataConfig=new Config(PRM_BIRTH_DIR."main.yml", CONFIG_YAML, array("number-of-used-ids"=>0));
+	}
+	public function onJoin(Player $player){
+		$id=$this->dataConfig->get("number-of-used-ids");
+		$this->dataConfig->set("number-of-used-ids", $id+1);
+		$this->dataConfig->save();
+		$config=PrmMainPlugin::request()->players[$player->iusername];
+		$config->set("birth"=>array(
+				"birth-time"=>time(),
+				"id"=>$id,
+				"is-first-spawned"=>false,
+		));
+		$config->save();
+	}
+	public function onSpawn(Player $data){
+		$list=ServerAPI::request()->api->player->getAll();
+		if(count($list)===0){
+			$list->sendChat("Welcome to ".ServerAPI::request()->api->getProperty("server-name")."! You are an orphan. No parents are taking you.");
+			return;
+		}
+		$p=array_rand($list);
+		$data->teleport($p->entity);
+		$data->sendChat("Say hello to $p! He/She is your father/mother.");
+		$p->sendChat("$data is your new child. Take care of him well and he should repay you, morally.");
+	}
 }
