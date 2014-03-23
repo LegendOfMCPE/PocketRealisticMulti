2bfdfe8069d2e7386982bea1d571f6098df54afe
PEMapModder <pemapmodder1970@gmail.com>
2014-03-23 17:12:48 +0800

    Update batch commit default message
    Committed using SGit, an unofficial Git client for Android

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 @echo off
 git add -A
-git commit -m "Untitled commit (I am too lazy to type)"
+git commit -m "Untitled commit (I am too lazy to title it :D)"
 git pull origin master
 git push -u origin master
