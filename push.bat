4ef6610a542bc271aa96a7e8ffa0515bc3e543a1
PEMapModder <pemapmodder1970@gmail.com>
2014-03-23 22:19:00 +0800

    Untitled commit (I am too lazy to title it :D) (This is an automatically-generated commit message.)

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 git add -A
-git commit -m "Untitled commit (I am too lazy to title it :D)\n\nThis is an automatically-generated commit message."
+git commit -m "Untitled commit (I am too lazy to title it :D) (This is an automatically-generated commit message.)"
 git pull origin master
 git push -u origin master
 pause
