19b2a2ccd8dff241f000f5f3124534b58c0a0ede
PEMapModder <pemapmodder1970@gmail.com>
2014-04-15 11:23:35 +0800

    Update Load.php

--- PocketRealisticMulti/src/prm/Load.php
+++ PocketRealisticMulti/src/prm/Load.php
@@ -3,7 +3,9 @@
 namespace prm;
 
 use pocketmine\plugin\PluginBase as Pb;
-use poxketmine\utils\Config;
+use pocketmine\utils\Config;
+
+use prm\datasaver\Players;
 
 class Load extends Pb{
 	public static $instance=false;
@@ -20,5 +22,20 @@ class Load extends Pb{
 			"physics"=>array("sound", "gravity", "mechanics"),
 			"biology"=>array("health"),
 			"society"=>array("economics", "lives")));
-		$this->loadList=new Config($ne.$ext, Config::YAML, $this->default);
+		$this->loadList=new Config($ne.$ext, Config::YAML);
+		if($this->loadList->getAll() === array()){
+			$this->loadList->setAll($this->default);
+		}
+	}
+	public function onEnable(){
+		$this->players=new Players();
+		foreach($this->loadList->get("load-list") as $categ=>$classes){
+			foreach($classes as $sclass){
+				$sclassname=ucfirst($sclass);
+				$className="prm\\$categ\\$sclassname";
+				$fieldname=$categ."_".$sclass;
+				$this->$fieldname=new $className();
+			}
+		}
+	}
 }
